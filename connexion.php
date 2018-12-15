bbf5e4dceb055934c89629d7b55333ff7ae6c501
SamuelFlan <39592050+SamuelFlan@users.noreply.github.com>
2018-12-15 18:23:02 +0100

    connexion
    ajout du code de test de la connexion + changement du nom de formulaire dans connexion.html

--- connexion.php
+++ connexion.php
@@ -3,7 +3,7 @@
 <div style=" position:relative; margin: auto; max-width: 75% ;width: auto;   border: 3px solid green;  padding: 70px 0; text-align: center ;background:rgba(250,250,250,0.7); color:black; overflow:auto">
 	<div id="logo" style="overflow:auto; margin-top: none"> <h1 style="position: relative; font-size: calc(12px + 3vw);display:left"> Ecole maternelle le Petit Prince  <img src="Images/petitprince1.jpg" alt="logo de l'ecole maternelle" style="position: relative ;width: auto; max-height:25vh"></h1> </div>
 	<h2 style="margin: 15px; font-size: calc(12px + 1.7vw)"> Connectez-vous  </h2> 
-	<form method="post"action="connexionPHP.php">
+	<form method="post"action="connexion_code.php">
 		<p>
 			Identifiant :
 			<input type="text"name="login" style = "margin:10px"/><br/>
