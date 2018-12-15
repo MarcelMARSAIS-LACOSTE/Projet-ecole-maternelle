9d4dc51a1541c7f0cc70f755e6dab011d65ea802
Tankadlavie <37742703+Tankadlavie@users.noreply.github.com>
2018-12-15 17:11:44 +0100

    Update and rename BDDStage.sql to BDDEcole.sql

---
+++ BDDEcole.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE IF NOT EXISTS PHP_Ecole;
+
+CREATE OR REPLACE TABLE Compte
+(
+	identifiant_compte VARCHAR(30),
+	mot_de_passe VARCHAR(50) NOT NULL,
+	etat_dacces INT, 
+	CONSTRAINT identifiant_personne_PK PRIMARY KEY(identifiant_compte)
+);
+
+CREATE TABLE Article
+(
+	identifiant_article VARCHAR(30) PRIMARY KEY,
+	titre VARCHAR(50),
+	etat_article INT, -- 1 : a valider 2 : a édtier 3 : publié 
+	comptes_concernes INT,
+	nombre_visite INT,
+	date_publication DATE,
+	date_rappel DATE,
+	texte VARCHAR(10000) -- Comprends toute la présentation 
+);
+
+CREATE TABLE Images
+(
+	nom_image VARCHAR(50),
+	chemin_image VARCHAR(100)
+);
+
