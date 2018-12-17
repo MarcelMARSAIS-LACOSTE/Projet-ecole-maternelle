c90ece40e8a5a56e8307e0932a4b46f9995da710
Tankadlavie <37742703+Tankadlavie@users.noreply.github.com>
2018-12-17 19:25:12 +0100

    Update affichage_directrice.php

--- affichage_directrice.php
+++ affichage_directrice.php
@@ -41,8 +41,7 @@ try
 				<tr>
 					<td><div id="titre_article"><?php echo $row['titre'];?></div>
 					<?php echo $row['date_publication']?>
-					<td><p><?php echo $row['texte'];     
-					echo $row['identifiant_article']; ?></p><td>
+					<td><p><?php echo $row['texte'];?></p><td>
 					<input type="hidden" name="foo" value="<?php echo $row['identifiant_article'];?>"/>
 					<input type="submit" name="val" value="Valider l'article"/>
 				</tr>
